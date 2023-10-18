eeb3fc612b9a0cb723f38e464589113aea2a1183
Matt <147024601+matthew-simms-298@users.noreply.github.com>
2023-10-18 00:01:06 -0600

    Quick Sort up created
    i created a quick sort algorithm, thats 1 down and 5 to go. I have array of integers as the array for now. Once we get the abstract class running we will need to change it around and make it reflect that.

--- src/sorts/QuickSort.java
+++ src/sorts/QuickSort.java
@@ -2,4 +2,35 @@ package sorts;
 
 public class QuickSort {
 
+    public void swapValues(int[] array, int index, int jndex) {
+        int placeHolder = array[index];
+        array[index] = array[jndex];
+        array[jndex] = placeHolder;
+
+    }
+
+
+     public int partitionArray(int leftBound, int rightBound, int[] array) {
+        int pivot = array[rightBound];
+
+        int index = leftBound - 1;
+
+        for (int jndex = leftBound; jndex <= rightBound - 1; jndex++) {
+            if (array[rightBound] < pivot) {
+                index++;
+                swapValues(array, index, jndex);
+            }
+        }
+        swapValues(array, index + 1, rightBound);
+        return index + 1;
+    }
+
+    public void quickSort(int[] array, int leftBound, int rightBound) {
+        if (leftBound < rightBound) {
+            int partition = partitionArray(leftBound, rightBound, array);
+
+            quickSort(array, leftBound, partition - 1);
+            quickSort(array, partition + 1, rightBound);
+        }
+    }
 }
