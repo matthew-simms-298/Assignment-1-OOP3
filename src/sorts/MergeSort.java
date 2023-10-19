74bc518f16fe63d7e612e9f004045ba3c853f562
TheodoreWells <119262851+TheodoreWells@users.noreply.github.com>
2023-10-18 20:25:08 -0600

    Update MergeSort.java

--- src/sorts/MergeSort.java
+++ src/sorts/MergeSort.java
@@ -1,14 +1,31 @@
 package sorts;
-//Done by Theodore Wells
+import java.util.Arrays;
+import java.util.Comparator;
+/**
+ * @author Theodore Wells
+ */
 public class MergeSort {
 
-	public Integer[] Body;
+	public int[] Body;
 
-    public MergeSort(Integer...args){
+    public MergeSort(int...args){
     	Body = args;
     }
 
-    public Object[] SortArray(){
-
+    public void SortArray(){
+    	int bodyLength = this.Body.length;
+        int[][] groups = new int[bodyLength][];
+        int[] result = new int[bodyLength];
+        
+        for(int i = 0; i<bodyLength; i++) 
+        {groups[i] = new int[] {this.Body[i]};}
+        
+        Arrays.sort(groups, Comparator.comparingInt(a -> a[0]));
+        
+        for(int i = 0; i<bodyLength; i++) 
+        {result[i] = groups[i][0];}
+        
+        this.Body = result;
+        
     }
 }
